0f0e2b9506e1dcf4f78387dea13a939daaf35613
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2025-07-10 21:24:16 +0300

    second commit

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 // namespace is a way to organize your code into logical groups
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$100,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Data Analyst',
-                'salary' => '$500,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Game Developer',
-                'salary' => '$800,000',
-            ]
-        ];
-    }
+class Job extends Model
+{
+    use HasFactory;
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array{
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(! $job){
-            abort(404, 'Job not found');
-        }
-        // If the job is not found, we abort with a 404 error
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/jobs', function (){
 });
 
 Route::get('/jobs/{id}', function ($id){
-    $job = job::find($id);
+    $job = Job::find($id);
 
     return view('job',['job' => $job]);
 });
